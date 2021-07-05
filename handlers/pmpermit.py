dca902f733326c1d9a35dc431a7d26f541ee7ee8
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-07-05 19:15:46 +0530

    Delete pmpermit.py

--- handlers/pmpermit.py
+++
@@ -1,9 +0,0 @@
-from callsmusic.callsmusic import client as USER
-from pyrogram import filters
-from pyrogram.types import Chat, Message, User
-
-
-@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
-async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"[✨](https://telegra.ph/file/f6fad124b1ffd3cf8eefb.png) Heya! This is the Music Assistant of [Musiqo](https://t.me/MusiqoRobot)")
-  return                        
