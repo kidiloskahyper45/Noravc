e8bca02cd020b39a96ca297431d4a26dba7c9a2b
Zyruz-ramu <77160871+Zyruz-ramu@users.noreply.github.com>
2021-07-06 22:59:05 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -97,7 +97,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
                 await f.close()
 
     image1 = Image.open("./background.png")
-    image2 = Image.open("image/foreground.png")
+    image2 = Image.open("image/-6280350636662959396_121.jpg")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
